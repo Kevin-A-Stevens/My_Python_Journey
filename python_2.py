6931e16cda84d7c825e2078b6ecc6f4ebaa4c0b1
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-03-13 21:39:40 -0500

    conditionals

--- python_2.py
+++ python_2.py
@@ -32,5 +32,100 @@ elif age == 900:
 else:
     print("You are old enough to vote")
 
+"""
+********** Guessing game **********
+"""
+answer = 5
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
 
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done! You guessed it.")
+    else:
+        print("Sorry, you have not guessed correctly.")
+else:
+    print("You got it first time!")
 
+"""
+********** Conditional Operators **********
+<  Less than
+<=  Less than or equal to
+>  Greater than
+>=  Greater than or equal to
+==  Equal to
+!-  Not equal to
+"""
+
+age = int(input("How old are you? "))
+
+# if age >= 16 and age <= 65:
+#     print("Have a good day at work.")
+
+"""
+********** Simplified Chain Comparison **********
+"""
+
+if 16 <= age <= 65:
+    print("Have a good day at work.")
+else:
+    print("Enjoy your free time")
+
+print("-" * 80)
+
+# if age < 16 or age > 65:
+#     print("Enjoy your free time")
+# else:
+#     print("Have a good day at work.")
+
+"""
+********** Boolean expressions **********
+True or False
+"""
+
+day = "Monday"
+temperature = 30
+raining = True
+
+if day == "Saturday" and temperature > 27 and not raining:
+    print("Go swimming")
+else:
+    print("Learn Python")
+
+if 0:
+    print("True")
+else:
+    print("False")
+
+name = input("Please enter your name: ")
+if name != "":
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man with no name?")
+
+"""
+********** in and not in **********
+"""
+
+parrot = "Norwegian Blue"
+letter = input("Enter a character: ")
+
+if letter in parrot:
+    print("{} is in {}".format(letter,parrot))
+else:
+    print("I don't need that letter")
+
+activity = input("What would you like to do today? ")
+
+## The casefold() function
+## Use casefold() rather than toLowerCase when you want to compare strings
+if "cinema" not in activity.casefold():
+    print("But I want to go to the cinema")
+
+#  https://docs.python.org/3/library/index.html
+#  https://docs.python.org/3/library/stdtypes.html

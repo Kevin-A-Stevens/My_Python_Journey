bb1e183cc476eeaeb649095fdb4165e5c4716e0e
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-03-14 22:28:23 -0500

    more loops

--- python_3.py
+++ python_3.py
@@ -95,3 +95,135 @@ for index in range(len(shopping_list)):
         break
 
 print("Item found at position {}".format(found_at))
+
+"""
+Initializing variables and None
+"""
+print("-" * 80)
+
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+
+item_to_find = "albatross"
+found_at = None
+
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
+        found_at = index  # Must be initialized outside of the for loop above
+        break
+
+if found_at is not None:
+    print("Item found at position {}".format(found_at))
+else:
+    print("{} not found".format(item_to_find))
+
+"""
+********** while loops **********
+"""
+print("-" * 80)
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1
+
+available_exits = ["north", "south", "east", "west"]
+
+chosen_exit = ""
+while chosen_exit not in available_exits:
+    chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() == "quit":
+        print("Game over")
+        break
+else:
+    print("I bet you are glad to be out of there")
+
+"""
+********** Guessing game **********
+"""
+print("-" * 80)
+
+"""
+The random module
+"""
+
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+guess = 0  # Initialize to any number that does not equal the answer
+print("Please guess a number between 1 and {}: ".format(highest))
+while guess != answer:
+    guess = int(input())
+
+    if guess == 0:
+        break
+    if guess == answer:
+        print("Well done! You guessed it.")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+
+"""
+********** Hi-Lo game **********
+The pass keyword = doesn't do anything but makes the code syntactically correct
+pass if often used as a place holder
+"""
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low, high))
+input("press ENTER to start")
+
+guesses = 1
+while low != high:
+    print("\tGuessing in the range of {} to {}".format(low, high))
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. Should I guess higher or lower? "
+                     "Enter h or l or c if my guess was correct "
+                     .format(guess)).casefold()
+
+    if high_low == "h":
+        # Guess higher. The low end of the range becomes 1 greater than the guess
+        # pass
+        low = guess + 1
+    elif high_low == "l":
+        # Guess lower. The high end of the range becomes one less than the guess
+        # pass
+        high = guess - 1
+    elif high_low == "c":
+        print("I got it in {} guesses!".format(guesses))
+        break
+    else:
+        print("Please enter h, l, or c")
+
+    guesses += 1
+else:
+    print("You thought of a number {}".format(low))
+    print("I got it in {} guesses".format(guesses))
+
+"""
+********** else in a loop **********
+Pay attention to the indentation
+The else is on the same level as the for loop
+"""
+numbers = [1, 45, 31, 12, 60]
+
+for number in numbers:
+    if number % 8 == 0:
+        print("The number is unacceptable")
+        break
+else:
+    print("All of the numbers are fine")
+
+"""
+********** Augmented assignment **********
+The combination in a single statement of a binary operation and an assignment 
+statement. Ex guesses += 1
+"""
+
+"""
+********** Style Guide PEP8 **********
+https://peps.python.org/pep-0008/
+"""

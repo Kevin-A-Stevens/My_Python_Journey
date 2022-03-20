8c757e86503e92888a1b9da1b3573c876f6cd873
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-03-20 16:05:59 -0500

    lists and list methods

--- python_4.py
+++ python_4.py
@@ -265,7 +265,6 @@ print(copy_numbers)
 
 """
 *********** Replacing an item in a list using slicing ***********
-.copy method
 """
 
 computer_parts = ["computer",
@@ -282,3 +281,76 @@ print(computer_parts)
 print(computer_parts[3:])
 computer_parts[3:] = ["trackball"]
 print(computer_parts)
+
+"""
+*********** Deleting an item from a list ***********
+"""
+
+print("-" * 80)
+data = [4,5,104,105,110,120,130,130,150,160,170,183,185,187,188,191,350,360]
+del data[0:2]
+print(data)
+del data[14:]
+print(data)
+
+"""
+*********** Deleting an item from a list ***********
+Testing your program
+"""
+print("-" * 80)
+data = [4,5,104,105,110,120,130,130,150,160,170,183,185,187,188,191,350,360]
+# data = [4,5,104,105,110,120,130,130,150,160,170,183,185,187,188,191]
+# data = [104,105,110,120,130,130,150,160,170,183,185,187,188,191,350,360]
+# data = [104,105,110,120,130,130,150,160,170,183,185,187,188,191]
+# data = [1041, 1051, 1101, 1201, 1301, 1501, 1601, 1701, 1831, 1851, 1871, 1881, 1911]
+# data = []
+
+min_valid = 100
+max_valid = 200
+
+# process the low values in the list
+stop = 0
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break
+print(stop) # For debugging
+del data[:stop]
+print(data)
+
+# process the high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_valid:
+        # We have the index of the last item to keep
+        # Set 'start' to the position of the first item to delete
+        # which is 1 after 'index'
+        start = index + 1
+        break
+print(start)  # For debugging
+del data[start:]
+print(data)
+
+"""
+*********** Removing items from a list backwards ***********
+reversed function
+"""
+data = [104, 101, 4, 105, 308, 103, 5, 107, 100, 306, 106, 102, 108]
+min_valid = 100
+max_valid = 200
+
+# for index in range(len(data) - 1, -1, -1):
+#     if data[index] < min_valid or data[index] > max_valid:
+#         print(index, data)
+#         del data[index]
+
+top_index = len(data) - 1
+for index, value in enumerate(reversed(data)):
+    if value < min_valid or value > max_valid:
+        print(top_index - index, value)
+        del data[top_index - index]
+print(data)
+
+
+
+

3f71db6b266cd905f5fecf09f8a75792735432a9
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-04-18 16:27:46 -0500

    binary and checking the hash of a file

--- python_11.py
+++ python_11.py
@@ -222,4 +222,26 @@ frame_types = {
     b'WXXX': 'User defined URL link frame',
 }
 
+"""
+Checking the hash of a file
+"""
+import hashlib
+
+published_hash = '9f47eda37229f68eee03b24b9748937c7dc3868f906e8ba69fbcbdd3bc5dc3e2'
+
+filename = 'colorama-0.4.4-py2.py3-none-any.whl'
+
+with open(filename, 'rb') as downloaded_file:
+    contents = downloaded_file.read()
+
+file_hash = hashlib.sha256(contents).hexdigest()
+print(file_hash)
+
+if file_hash != published_hash:
+    print(f'The file {filename} has been modified')
+else:
+    print(f'file {filename} hash is correct')
+
+9
+
 

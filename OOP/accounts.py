091fd0c9c3cefecfcee10c41defe01b5b21a4159
Kevin-A-Stevens <kevin.alan.stevens@gmail.com>
2022-05-03 21:44:52 -0500

    methods

---
+++ OOP/accounts.py
@@ -0,0 +1,61 @@
+import datetime
+import pytz
+
+class Account:
+    """ Simple account class with balance """
+
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        self.transaction_list = []
+        print("Account created for " + self.name)
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            self.show_balance()
+            self.transaction_list.append((Account._current_time(), amount))
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            self.transaction_list.append((Account._current_time(), -amount))
+        else:
+            print("The amount must be greater than 0 and no more than your account balance")
+        self.show_balance()
+
+    def show_balance(self):
+        print("Balance is {}".format(self.balance))
+
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1
+            print("{:6} {} on {} (local time was {})".format(amount, tran_type, date, date.astimezone()))
+
+
+""" 
+Now that the class is defined, we can start 
+adding people to add cash to their accounts
+"""
+
+if __name__ == '__main__':
+    kevin = Account('Kevin', 0)
+    kevin.show_balance()
+
+    kevin.deposit(1000)
+    # kevin.show_balance()
+    kevin.withdraw(500)
+    # kevin.show_balance()
+
+    kevin.withdraw(2000)
+
+    kevin.show_transactions()
